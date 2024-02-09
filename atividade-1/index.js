c34f6524a84b2b144a05727c894c4a6c535473e9
Luis Armando <126887300+LuisinhoTheKid@users.noreply.github.com>
2024-02-09 12:09:55 -0300

    Add files via upload

---
+++ atividade-1/index.js
@@ -0,0 +1,13 @@
+  let contador = 0;
+  let parada = "stop";
+
+  //loop usando WHILE
+  while (contador <10){
+    console.log('Loop While' - $ {contador});
+    contador++;
+}
+
+ddo{
+    console.log('Loop Do - ${contador}')
+    contador++
+}while(contador<10);
